d48d172e8acb96ba180a66f60babef7882c4656d
Filaret Crainiciuc <81650683+Filaret2000@users.noreply.github.com>
2024-11-27 15:21:58 +0200

    Updated front url

--- FitnessAPI/Program.cs
+++ FitnessAPI/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngularDev",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("https://fitnessppaw.netlify.app")
                   .AllowAnyMethod()
                   .AllowAnyHeader()
                   .AllowCredentials();
